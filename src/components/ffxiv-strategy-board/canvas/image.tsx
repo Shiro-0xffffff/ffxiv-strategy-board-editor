549bea1b71dd36f2dcd870d46ab1ab8f8cf56736
Shiro-0xffffff <shiro.0xffffff@gmail.com>
2026-01-07 17:45:18 +0800

    feat: 重复图案展示

--- src/components/ffxiv-strategy-board/canvas/image.tsx
+++ src/components/ffxiv-strategy-board/canvas/image.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { Group, Image } from 'react-konva'
+import { Group, Rect, Image } from 'react-konva'
 import useImage from 'use-image'
-import { StrategyBoardCommonObject, StrategyBoardMechanicLineStackObject, StrategyBoardMechanicLinearKnockbackObject } from '@/lib/ffxiv-strategy-board'
+import { StrategyBoardCommonObject, StrategyBoardMechanicLineStackObject, StrategyBoardMechanicLinearKnockbackObject, StrategyBoardObjectType } from '@/lib/ffxiv-strategy-board'
 import { ffxivImageUrl } from '@/lib/utils'
 
 import { objectLibrary } from '../constants'
@@ -16,6 +16,16 @@ export interface ImageCanvasObjectProps {
 export function ImageCanvasObject(props: ImageCanvasObjectProps) {
   const { object } = props
   const { type, size, flipped, rotation, transparency } = object
+
+  const repeat: { x: number, y: number } | null = (object => {
+    if (object.type === StrategyBoardObjectType.MechanicLineStack) {
+      return { x: 1, y: object.displayCount }
+    }
+    if (object.type === StrategyBoardObjectType.MechanicLinearKnockback) {
+      return { x: object.displayCount.horizontal, y: object.displayCount.vertical }
+    }
+    return null
+  })(object)
   
   const objectLibraryItem = objectLibrary.get(type)!
 
@@ -33,14 +43,26 @@ export function ImageCanvasObject(props: ImageCanvasObjectProps) {
         scaleX={flipped ? -1 : 1}
         rotation={rotation}
       >
-        <Image
-          offsetX={imageSize.width / 2}
-          offsetY={imageSize.height / 2}
-          width={imageSize.width}
-          height={imageSize.height}
-          image={backgroundImage}
-          alt={objectLibraryItem.abbr}
-        />
+        {repeat ? (
+          <Rect
+            offsetX={imageSize.width * repeat.x / 2}
+            offsetY={imageSize.height * repeat.y / 2}
+            width={imageSize.width * repeat.x}
+            height={imageSize.height * repeat.y}
+            fillPatternImage={backgroundImage}
+            fillPatternScaleX={backgroundImage?.width ? imageSize.width / backgroundImage.width : 1}
+            fillPatternScaleY={backgroundImage?.height ? imageSize.height / backgroundImage.height : 1}
+          />
+        ) : (
+          <Image
+            offsetX={imageSize.width / 2}
+            offsetY={imageSize.height / 2}
+            width={imageSize.width}
+            height={imageSize.height}
+            image={backgroundImage}
+            alt={objectLibraryItem.abbr}
+          />
+        )}
       </Group>
     </>
   )
